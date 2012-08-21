fbe0905d87b28114af397daeb3b1a627d85ff40e
Kevin Mershon <nwlinkvxd@gmail.com>
2012-08-21 21:28:43 +0000

    Started building out the user controller

---
+++ grails-app/controllers/org/torpidity/core/UserController.groovy
@@ -0,0 +1,47 @@
+package org.torpidity.core
+
+class UserController {
+
+    def scope = "singleton"
+
+    /**
+     * Show the login page
+     */
+    def login() {
+        // TODO -- Redirect to the logged in page
+        if (session.user) {
+            // go somewhere
+            //redirect(controller: "afterLoginController", action: "action")
+        }
+    }
+
+    /**
+     * Try to login with the specified email and password
+     */
+    def doLogin() {
+        // Find the user with the specified email
+        def user = User.findByEmail(params["username"])
+
+        // Validate the password
+        def hashedPassword = (params["password"] + user.passwordSalt +
+            params["email"]).encodeAsSHA1()
+
+        if (hashedPassword == user.passwordHash) {
+            // Redirect to the logged in page
+            session.user = user
+            redirect(action: "login")
+        } else {
+            // Redirect back to the login screen, with an error message
+            flash.loginFailed = true
+            redirect(controller: "user", action: "login")
+        }
+    }
+
+    /**
+     * Destroy the user's session and redirect to the login page
+     */
+    def logout() {
+        session.invalidate()
+        redirect(controller: "user", action: "login")
+    }
+}
