156a725b1938ff7c7f361199a52b61c653af9a9a
Kevin Mershon <nwlinkvxd@gmail.com>
2012-08-21 22:13:12 +0000

    Updated the dummy users definition to use UserService to assign the password salt/hash

--- src/groovy/org/torpidity/core/DummyUsers.groovy
+++ src/groovy/org/torpidity/core/DummyUsers.groovy
@@ -7,12 +7,14 @@ class DummyUsers {
         if (isSetUp)
             return
 
+        def userService = new UserService()
+
         // Admin user
         def admin = new User(
             alias: "admin",
             email: "admin@torpidity.org"
         )
-        admin.changePassword("ALSO, THE GAME")
+        userService.changePassword(admin, "ALSO, THE GAME")
         admin.save(flush: true, failOnError: true)
 
         // prevent redundant definitions in testing
